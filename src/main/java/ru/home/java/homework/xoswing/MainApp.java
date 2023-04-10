d6ca3b6d1fd849320f219a400a3c448c4e0682c9
whata <whatacrap@gmail.com>
2023-04-10 21:59:58 +0300

    + MainApp, MyGameWindow

---
+++ src/main/java/ru/home/java/homework/xoswing/MainApp.java
@@ -0,0 +1,7 @@
+package ru.home.java.homework.xoswing;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new MyGameWindow();
+    }
+}
---
+++ src/main/java/ru/home/java/homework/xoswing/MyGameWindow.java
@@ -0,0 +1,12 @@
+package ru.home.java.homework.xoswing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MyGameWindow extends JFrame {
+    public MyGameWindow() {
+
+    }
+}
